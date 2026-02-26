05e9e84f419f05826f44d043fc7493cbbcd2cdf0
Mayeu <m@mayeu.me>
2026-02-26 21:44:44 +0100

    fix: wrap namaka check in derivation for nix build
    namaka.lib.load returns {} on success, which isn't a derivation.
    Wrap in runCommand so nix build .#checks.<system> works.

--- src/tests/checks.nix
+++ src/tests/checks.nix
@@ -1,16 +1,22 @@
 let
-  inherit (inputs) namaka self;
+  inherit (inputs) namaka nixpkgs self;
   inputs' = builtins.removeAttrs inputs ["self"];
+  namakaResults = namaka.lib.load {
+    src = self + /tests;
+    inputs =
+      inputs'
+      //
+      # inputs.self is too noisy for 'check-augmented-cell-inputs'
+      {inputs = inputs';};
+  };
 in {
   snapshots = {
     meta.description = "The main Standard Snapshotting test suite";
-    check = namaka.lib.load {
-      src = self + /tests;
-      inputs =
-        inputs'
-        //
-        # inputs.self is too noisy for 'check-augmented-cell-inputs'
-        {inputs = inputs';};
-    };
+    # 2026-03-01 @mayeu: unsure if that's ok, but namaka.lib.load throws on
+    # failure, returns {} on success. Wrap in a derivation so nix build
+    # .#checks.<system> works.
+    check = nixpkgs.runCommand "namaka-snapshots" {} ''
+      # ${builtins.toJSON namakaResults}
+      touch $out '';
   };
 }
